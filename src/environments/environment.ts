9a8e6f9e814756070a8851bd13fad7f70f708901
Rodrigo Nascimento <rodrigo.nascimento+kraken@zenklub.com>
2021-08-31 08:31:12 -0300

    cleaning firebase from project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  useEmulators: true,
   firebase: {
     apiKey: "AIzaSyBBsmc_seBCdBBDPfDskYKQAtXyJLVoOtg",
     authDomain: "fir-course-recording-be876.firebaseapp.com",
