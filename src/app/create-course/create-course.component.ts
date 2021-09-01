58464defbe4df565484c899bce103122c1e3c973
Rodrigo Nascimento <rodrigo.nascimento+kraken@zenklub.com>
2021-09-01 09:02:06 -0300

    create CRUD

--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -1,27 +1,71 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {AngularFirestore} from '@angular/fire/firestore';
-import {Course} from '../model/course';
-import {catchError, concatMap, last, map, take, tap} from 'rxjs/operators';
-import {from, Observable, throwError} from 'rxjs';
-import {Router} from '@angular/router';
-import {AngularFireStorage} from '@angular/fire/storage';
-import firebase from 'firebase/app';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Course } from "../model/course";
+import { catchError, concatMap, last, map, take, tap } from "rxjs/operators";
+import { from, Observable, throwError } from "rxjs";
+import { Router } from "@angular/router";
+import { AngularFireStorage } from "@angular/fire/storage";
+import firebase from "firebase/app";
 import Timestamp = firebase.firestore.Timestamp;
+import { CoursesService } from "../services/courses.service";
 
 @Component({
-  selector: 'create-course',
-  templateUrl: 'create-course.component.html',
-  styleUrls: ['create-course.component.css']
+  selector: "create-course",
+  templateUrl: "create-course.component.html",
+  styleUrls: ["create-course.component.css"],
 })
 export class CreateCourseComponent implements OnInit {
+  courseId: string;
 
-  constructor() {
+  form = this.fb.group({
+    description: ["", Validators.required],
+    category: ["BEGGINER", Validators.required],
+    url: ["", Validators.required],
+    longDescription: ["", Validators.required],
+    promo: [false],
+    promoStart: [null],
+  });
 
-  }
+  constructor(
+    private fb: FormBuilder,
+    private courseService: CoursesService,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-
+    this.courseId = this.afs.createId();
   }
 
+  onCreateCourse() {
+    const formValues = this.form.value;
+
+    const newCourse: Partial<Course> = {
+      description: formValues.description,
+      url: formValues.url,
+      longDescription: formValues.longDescription,
+      promo: formValues.promo,
+      categories: [formValues.category],
+    };
+
+    newCourse.promoStartAt = Timestamp.fromDate(this.form.value.promoStart);
+
+    console.table(newCourse);
+
+    this.courseService
+      .createCourse(newCourse, this.courseId)
+      .pipe(
+        tap((course) => {
+          console.log("New created course", course);
+          this.router.navigateByUrl("/courses");
+        }),
+        catchError((error) => {
+          console.error(error);
+          alert("we could not create the course");
+          return throwError(error);
+        })
+      )
+      .subscribe();
+  }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
 import { AngularFirestore } from "@angular/fire/firestore";
-import { map } from "rxjs/operators";
+import { concatMap, map } from "rxjs/operators";
 import { Course } from "../model/course";
 import { convertSnaps } from "../services/db-utils";
-import { Observable } from "rxjs";
+import { Observable, from } from "rxjs";
 
 @Injectable({
   providedIn: "root",
@@ -18,5 +18,40 @@ export class CoursesService {
       )
       .get()
       .pipe(map((result) => convertSnaps<Course>(result)));
+  }
+
+  createCourse(newCourse: Partial<Course>, courseId?: string) {
+    return this.db
+      .collection("courses", (ref) => ref.orderBy("seqNo", "desc").limit(1))
+      .get()
+      .pipe(
+        concatMap((result) => {
+          const courses = convertSnaps<Course>(result);
+
+          const lastCourseSeqNo = courses[0]?.seqNo ?? 0;
+
+          const course = {
+            ...newCourse,
+            seqNo: lastCourseSeqNo + 1,
+          };
+
+          let save$: Observable<any>;
+
+          if (courseId) {
+            save$ = from(this.db.doc(`courses/${courseId}`).set(course));
+          } else {
+            save$ = from(this.db.collection("courses").add(course));
+          }
+
+          return save$.pipe(
+            map((respose) => {
+              return {
+                id: courseId ?? respose.id,
+                ...course,
+              };
+            })
+          );
+        })
+      );
   }
 }
